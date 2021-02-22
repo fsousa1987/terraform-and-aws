a78ff072215a5a0c16ae45254453ec0c626a5377
Francisco Sousa <franciscoeds1987@gmail.com>
2021-02-22 10:31:35 -0300

    Dependências entre os recursos

--- main.tf
+++ main.tf
@@ -10,7 +10,28 @@ resource "aws_instance" "dev" {
   tags = {
     "Name" = "dev ${count.index}"
   }
-  vpc_security_group_ids = [ "sg-0031123d255755063" ]
+  vpc_security_group_ids = [ aws_security_group.acesso-ssh.id ]
+}
+
+resource "aws_instance" "dev3" {
+  ami = "ami-0996d3051b72b5b2c"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    "Name" = "dev 3"
+  }
+  vpc_security_group_ids = [ aws_security_group.acesso-ssh.id ]
+}
+
+resource "aws_instance" "dev4" {
+  ami = "ami-0996d3051b72b5b2c"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    "Name" = "dev 4"
+  }
+  vpc_security_group_ids = [ aws_security_group.acesso-ssh.id ]
+  depends_on = [ aws_s3_bucket.dev3 ]
 }
 
 resource "aws_security_group" "acesso-ssh" {
@@ -27,5 +48,14 @@ resource "aws_security_group" "acesso-ssh" {
 
   tags = {
     Name = "ssh"
+  }
+}
+
+resource "aws_s3_bucket" "dev3" {
+  bucket = "franciscoedslabs-dev3"
+  acl    = "private"
+
+  tags = {
+    Name = "franciscoedslabs-dev3"
   }
 }
