16b2cf5889622ada3710ae099e55e9bde976d982
Francisco Sousa <franciscoeds1987@gmail.com>
2021-02-23 14:11:58 -0300

    Novos recursos em diferentes regiões

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-2"
 }
 
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+}
+
 resource "aws_instance" "dev" {
   count = 3
   ami = "ami-0996d3051b72b5b2c"
@@ -31,6 +36,18 @@ resource "aws_instance" "dev4" {
     "Name" = "dev 4"
   }
   vpc_security_group_ids = [ aws_security_group.acesso-ssh.id ]
+  depends_on = [ aws_s3_bucket.dev3 ]
+}
+
+resource "aws_instance" "dev5" {
+  provider = aws.us-east-1
+  ami = "ami-047a51fa27710816e"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    "Name" = "dev 5"
+  }
+  vpc_security_group_ids = [ aws_security_group.acesso-ssh-us-east-1.id ]
   depends_on = [ aws_s3_bucket.dev3 ]
 }
 
--- security-group.tf
+++ security-group.tf
@@ -14,3 +14,21 @@ resource "aws_security_group" "acesso-ssh" {
     Name = "ssh"
   }
 }
+
+resource "aws_security_group" "acesso-ssh-us-east-1" {
+  provider = aws.us-east-1
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    description = "acesso-ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["45.224.199.47/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
